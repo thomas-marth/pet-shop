650d9d92aab6d8167256d27e818dbe8cd454bcc7
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-09-05 21:36:43 +0200

    Implemented scrollToTop component

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import ScrollToTop from "./components/scrollToTop";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
+      <ScrollToTop />
       <App />
     </Provider>
   </BrowserRouter>
