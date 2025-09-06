c8ab9407533edc306de0f7fb2bb26ee10ab77dc6
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-09-06 15:37:13 +0200

    Added and styled product cards for the SaleSection component

--- src/components/saleSection/index.jsx
+++ src/components/saleSection/index.jsx
@@ -1,13 +1,125 @@
-import SectionHeader from "../sectionHeader";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Box,
+  Typography,
+} from "@mui/material";
 
-const SalesSection = () => {
+import SectionHeader from "../sectionHeader";
+import { fetchProducts } from "@/redux/slices/productSlice";
+import { CONFIG } from "@/shared/config";
+import styles from "./styles.module.css";
+
+const SaleSection = () => {
+  const dispatch = useDispatch();
+  const { items } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    if (!items.length) {
+      dispatch(fetchProducts());
+    }
+  }, [dispatch, items.length]);
+
+  const discountedProducts = items
+    .filter((p) => p.discont_price && p.discont_price < p.price)
+    .map((p) => ({
+      ...p,
+      discount: Math.round(((p.price - p.discont_price) / p.price) * 100),
+    }))
+    .sort((a, b) => b.discount - a.discount)
+    .slice(0, 4);
+
   return (
-    <section>
+    <section className={styles.section}>
       <div className="container">
         <SectionHeader title="Sale" to="/sales" buttonText="All sales" />
+        <ul className={styles.list}>
+          {discountedProducts.map((product) => {
+            const imgSrc = product.image?.startsWith("http")
+              ? product.image
+              : `${CONFIG.API_URL}/${product.image}`;
+            return (
+              <li key={product.id} className={styles.item}>
+                <Card
+                  className={styles.card}
+                  sx={{
+                    border: "1px solid #ddd",
+                    borderRadius: "12px",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    boxShadow: "none",
+                  }}
+                >
+                  <Box className={styles.badge}>-{product.discount}%</Box>
+                  {/* <CardActionArea> */}
+                  <CardMedia
+                    component="img"
+                    image={imgSrc}
+                    alt={product.title}
+                    className={styles.image}
+                  />
+                  <CardContent
+                    sx={{
+                      padding: "20px 32px 32px 32px",
+                      borderTop: "1px solid #ddd",
+                    }}
+                  >
+                    <Typography
+                      component="h3"
+                      className={styles.name}
+                      sx={{
+                        color: "#282828",
+                        fontFamily: "Montserrat",
+                        fontSize: "20px",
+                        fontWeight: "500",
+                        lineHeight: "1.3",
+                        marginBottom: "16px",
+                      }}
+                    >
+                      {product.title}
+                    </Typography>
+                    <div className={styles.prices}>
+                      <Typography
+                        component="span"
+                        className={styles.newPrice}
+                        sx={{
+                          color: "#282828",
+                          fontFamily: "Montserrat",
+                          fontSize: "40px",
+                          fontWeight: "600",
+                          lineHeight: "1.1",
+                        }}
+                      >
+                        ${product.discont_price}
+                      </Typography>
+                      <Typography
+                        component="span"
+                        className={styles.oldPrice}
+                        sx={{
+                          fontFamily: "Montserrat",
+                          fontSize: "20px",
+                          fontWeight: "500",
+                          lineHeight: "1.3",
+                        }}
+                      >
+                        ${product.price}
+                      </Typography>
+                    </div>
+                  </CardContent>
+                  {/* </CardActionArea> */}
+                </Card>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </section>
   );
 };
 
-export default SalesSection;
+export default SaleSection;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoryReducer from "./slices/categorySlice";
+import productReducer from "./slices/productSlice";
 
 const store = configureStore({
   reducer: {
     categories: categoryReducer,
+    products: productReducer,
   },
 });
 
