269bf51e4d4abd9f41ef6ad9d5d53071df0779b9
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-09-07 20:43:10 +0200

    sectionHeading styles refactoring

--- src/components/sectionHeading/index.jsx
+++ src/components/sectionHeading/index.jsx
@@ -9,25 +9,10 @@ const SectionHeading = ({ title, to, buttonText }) => {
       <div className={styles.buttonWrapper}>
         <div className={styles.divider} />
         <Button
+          className={`${styles.button} ${styles.mui}`}
           component={NavLink}
           to={to}
           variant="outlined"
-          sx={{
-            color: "#8B8B8B",
-            borderColor: "#DDD",
-            fontFamily: "Montserrat",
-            fontSize: "16px",
-            fontWeight: 500,
-            lineHeight: 1.3,
-            textTransform: "none",
-            borderRadius: "6px",
-            marginTop: "9px",
-            transition: "all 0.3s",
-            "&:hover": {
-              backgroundColor: "#282828",
-              color: "#fff",
-            },
-          }}
         >
           {buttonText}
         </Button>
