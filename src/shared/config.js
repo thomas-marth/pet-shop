6acc126ac924d5f0893a68f1e61ad486f19aa10a
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-09-07 17:12:49 +0200

    Changed base url

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,3 +1,5 @@
 export const CONFIG = {
-  API_URL: import.meta.env.VITE_API_URL ?? "http://localhost:3333",
+  API_URL:
+    import.meta.env.VITE_API_URL ??
+    "https://petshop-project-backend.vercel.app/",
 };
