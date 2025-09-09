ccd9174524abd87ba7e638904f1b374004b7c4a4
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-09-09 20:18:28 +0200

    Added badge for the Cart icon

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Badge from "@mui/material/Badge";
 import ShoppingBag from "../../assets/icons/Vector.png";
 import Logo from "../../assets/icons/logo.svg";
 import styles from "./styles.module.css";
 
 const Header = () => {
+  const itemCount = useSelector((state) => state.cart.items.length);
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -48,7 +52,24 @@ const Header = () => {
         </nav>
         {/* Cart icon */}
         <NavLink to="/cart" className={styles.cartLink} aria-label="Cart">
-          <img className={styles.cart} src={ShoppingBag} alt="Shopping Bag" />
+          <Badge
+            className={`${styles.badge} ${styles.mui} ${styles.mui2}`}
+            badgeContent={itemCount}
+            color="primary"
+            sx={{
+              "& .MuiBadge-badge": {
+                color: "#fff",
+                textAlign: "center",
+                fontFamily: "Montserrat",
+                fontSize: "12px",
+                fontWeight: 600,
+                lineHeight: 0.9,
+                padding: "7px",
+              },
+            }}
+          >
+            <img className={styles.cart} src={ShoppingBag} alt="Shopping Bag" />
+          </Badge>
         </NavLink>
       </div>
     </header>
