7f2c118d03e4d706c6e3f3610a8af34a9cba0115
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-09-11 12:05:51 +0200

    Changed imports in App.jsx for the deploying on netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
+import MainLayout from "@/layouts/MainLayout";
 
 // Pages
 import HomePage from "./pages/home";
